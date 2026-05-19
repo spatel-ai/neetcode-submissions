f0ca1394a4fe8cdb2919fda918b7b9b924e71454
Shubham Patel <75959964+spatel-ai@users.noreply.github.com>
2026-05-20 01:09:02 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> seen = new HashMap<>();
+        int[] freq = new int[26];
+        for (String word : strs) {
+            Arrays.fill(freq, 0);
+            for (char ch : word.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+            String key = Arrays.toString(freq).replaceAll(" ", "");
+            if (!seen.containsKey(key)) {
+                seen.put(key, new ArrayList<>());
+            }
+            seen.get(key).add(word);
+        }
+
+        for(List<String> l : seen.values()){
+            result.add(l);
+        }
+        return result;
+    }
+}
